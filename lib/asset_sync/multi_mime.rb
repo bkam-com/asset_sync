6ef7a889501b654d47a0b3ffb75bf7ca38dbc988
Mahmoud Abdel-Fattah <accounts@abdel-fattah.net>
2015-01-06 20:44:34 +0200

    now looks better!

--- lib/asset_sync/multi_mime.rb
+++ lib/asset_sync/multi_mime.rb
@@ -4,11 +4,12 @@ module AssetSync
   class MultiMime
 
     def self.lookup(ext)
-
+      
+      ext_with_dot = ".#{ext}"
+      
       if defined?(Mime::Type) and !Mime::Type.lookup_by_extension(ext).nil?
         Mime::Type.lookup_by_extension(ext)
-      elsif defined?(Rack::Mime) and !Rack::Mime.mime_type(".#{ext}").nil?
-        ext_with_dot = ".#{ext}"
+      elsif defined?(Rack::Mime) and !Rack::Mime.mime_type(ext_with_dot).nil?
         Rack::Mime.mime_type(ext_with_dot)
       else
         ::MIME::Types.type_for(ext).first
